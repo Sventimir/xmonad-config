8dae1fb9cdc8d9ea687191df6bd32efb5b293fc4
Sventimir <svantimir@gmail.com>
2025-03-18 17:40:12 +0100

    Add type signatures in main module

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,7 @@
+import Data.Semigroup (All)
+import GHC.IO.Handle.Types (Handle)
+import qualified Graphics.X11.Xlib as Xlib
+import qualified Graphics.X11.Xlib.Extras as Xlib.Extras
 import System.IO (hPutStrLn)
 import XMonad (X, XConfig(..), ManageHook, xmonad, mod4Mask,
                 spawn, kill, windows, composeAll, withFocused, className,
@@ -5,6 +9,8 @@ import XMonad (X, XConfig(..), ManageHook, xmonad, mod4Mask,
 import qualified XMonad.Config as Cfg
 import qualified XMonad.Hooks.DynamicLog as DynLog
 import qualified XMonad.Hooks.ManageDocks as Docks
+import qualified XMonad.Layout as Layout
+import qualified XMonad.Layout.LayoutModifier as LayoutModifier
 import qualified XMonad.Util.EZConfig as EZConf
 import qualified XMonad.Util.Run as XMRun
 import qualified XMonad.StackSet as StackSet
@@ -15,10 +21,19 @@ import qualified Network.MPD as MPD
 import Custom
 
 
+main :: IO ()
 main = do
     readEnd <- XMRun.spawnPipe "xmobar"
     xmonad $ EWMH.ewmh $ configWith readEnd
 
+configWith :: GHC.IO.Handle.Types.Handle
+           -> XConfig
+           (LayoutModifier.ModifiedLayout
+             Docks.AvoidStruts
+             (Layout.Choose
+              Layout.Tall
+               (Layout.Choose
+                (Layout.Mirror Layout.Tall) Layout.Full)))
 configWith xmproc = Cfg.def {
             modMask     = mod4Mask,      -- Win as mod-key
             terminal    = terminalCmd,
@@ -30,8 +45,11 @@ configWith xmproc = Cfg.def {
             startupHook = startup
         } `EZConf.additionalKeysP` keyBindings
 
+
+eventHook :: Xlib.Extras.Event -> X All
 eventHook = mconcat [ EWMH.fullscreenEventHook, Docks.docksEventHook, handleEventHook Cfg.def ]
 
+logHookWith :: Handle -> X ()
 logHookWith xmproc = DynLog.dynamicLogWithPP DynLog.xmobarPP {
         DynLog.ppOrder  = \(_ : _ : title) -> title,
         DynLog.ppOutput = hPutStrLn xmproc,
