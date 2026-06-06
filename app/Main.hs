89206f6a056e6a1dc468c72f0d14b837f2f8a950
Sventimir <svantimir@gmail.com>
2026-06-06 16:44:30 +0200

    Change the way screens are configured for XMonad

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,7 @@ import GHC.IO.Handle.Types (Handle)
 import qualified Graphics.X11.Xlib as Xlib
 import qualified Graphics.X11.Xlib.Extras as Xlib.Extras
 import System.IO (hPutStrLn)
-import XMonad (X, XConfig(..), ManageHook, xmonad, mod4Mask,
-                spawn, kill, windows, composeAll, withFocused, className,
-                ask, doShift, (-->), (=?))
+import XMonad (X, XConfig(..), xmonad, mod4Mask) 
 import qualified XMonad.Config as Cfg
 import qualified XMonad.Hooks.DynamicLog as DynLog
 import qualified XMonad.Hooks.ManageDocks as Docks
@@ -13,18 +11,17 @@ import qualified XMonad.Layout as Layout
 import qualified XMonad.Layout.LayoutModifier as LayoutModifier
 import qualified XMonad.Util.EZConfig as EZConf
 import qualified XMonad.Util.Run as XMRun
-import qualified XMonad.StackSet as StackSet
 import XMonad.Hooks.EwmhDesktops as EWMH
 
-import qualified Control.Keyboard as Kbd
-import qualified Network.MPD as MPD
 import Custom
 
 
 main :: IO ()
 main = do
-    readEnd <- XMRun.spawnPipe "xmobar"
-    xmonad $ EWMH.ewmh $ configWith readEnd
+  display <- Xlib.openDisplay ":0"
+  Xlib.closeDisplay display
+  readEnd <- XMRun.spawnPipe "xmobar"
+  xmonad $ EWMH.ewmh $ configWith readEnd
 
 configWith :: GHC.IO.Handle.Types.Handle
            -> XConfig
@@ -51,7 +48,7 @@ eventHook = mconcat [ EWMH.fullscreenEventHook, Docks.docksEventHook, handleEven
 
 logHookWith :: Handle -> X ()
 logHookWith xmproc = DynLog.dynamicLogWithPP DynLog.xmobarPP {
-        DynLog.ppOrder  = \(_ : _ : title) -> title,
+        DynLog.ppOrder  = \(_ : _ : title) -> take 30  title,
         DynLog.ppOutput = hPutStrLn xmproc,
         DynLog.ppTitle  = DynLog.xmobarColor "green" "" . DynLog.shorten 50
     }
--- src/Control/MPD.hs
+++ src/Control/MPD.hs
@@ -3,15 +3,15 @@ module Control.MPD (
   mpdPlay
 ) where
 
+import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO)
 import qualified Network.MPD as MPD
-import Network.MPD (MPD, Status(..), PlaybackState(..), status, play,
-                    toggle, stop)
+import Network.MPD (MPD, Status(..), PlaybackState(..), status, play, toggle)
 import XMonad (X)
 
 
 withMPD :: MPD () -> X ()
-withMPD mpdAction = liftIO (MPD.withMPD mpdAction >> return ())
+withMPD mpdAction = liftIO . void $ MPD.withMPD mpdAction
 
 mpdPlay :: MPD ()
 mpdPlay = do
